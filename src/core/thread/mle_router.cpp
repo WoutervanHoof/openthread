e3d189fd63c4fdc35cc1f827c4c9513d8c9b18ae
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-03-24 15:47:32 +0100

    fix: check mServerDataLength instead of Length()

--- src/core/thread/mle_router.cpp
+++ src/core/thread/mle_router.cpp
@@ -2219,11 +2219,13 @@ Error MleRouter::ProcessMUDUrl(String<kMUDUrlMaxLength> aMUDUrl, const Child *ne
             LogInfo("reading ipv6 string");
             service.GetServerConfig().GetServerData(serverData);
             // Get IPv6 address from serverdata
-            if (serverData.GetLength() >= Ip6::Address::kInfoStringSize) {
+            if (service.GetServerConfig().mServerDataLength >= Ip6::Address::kInfoStringSize) {
+                LogWarn("serverdatalenght %d >= ip6 infostringsize %d ", service.GetServerConfig().mServerDataLength, Ip6::Address::kInfoStringSize);
                 ExitNow(error = kErrorInvalidState);
             }
 
             serverAddressString.AppendHexBytes(serverData.GetBytes(), serverData.GetLength());
+            LogInfo("parsing string %s", serverAddressString.AsCString());
             SuccessOrExit(error = serverAddress.FromString(serverAddressString.AsCString()));
 
             LogInfo("read ipv6 address %s", serverAddress.ToString().AsCString());
