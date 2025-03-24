520492a9cb8eb4b649904712a34c4b7d9ea4ba7a
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-03-24 16:04:59 +0100

    fix: should have been > not >= but now in actual code, not log message

--- src/core/thread/mle_router.cpp
+++ src/core/thread/mle_router.cpp
@@ -2219,7 +2219,7 @@ Error MleRouter::ProcessMUDUrl(String<kMUDUrlMaxLength> aMUDUrl, const Child *ne
             LogInfo("reading ipv6 string");
             service.GetServerConfig().GetServerData(serverData);
             // Get IPv6 address from serverdata
-            if (service.GetServerConfig().mServerDataLength >= Ip6::Address::kInfoStringSize) {
+            if (service.GetServerConfig().mServerDataLength > Ip6::Address::kInfoStringSize) {
                 LogWarn("serverdatalenght %d > ip6 infostringsize %d ", service.GetServerConfig().mServerDataLength, Ip6::Address::kInfoStringSize);
                 ExitNow(error = kErrorInvalidState);
             }
